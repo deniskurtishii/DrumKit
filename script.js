bfa21bb824c3b5b4d282d0380da4cd4295eb287d
deniskurtishii <denis.kurtishii@hotmail.com>
2023-08-11 12:03:01 +0200

    DrumKit

---
+++ script.js
@@ -0,0 +1,28 @@
+let crashRide = document.querySelector("#crash-ride");
+let hiHatTop = document.querySelector("#hihat-top");
+const animateCrashOrRide = () => {
+  crashRide.style.transform = "rotate(0deg) scale (1.5)";
+};
+
+const animateHiHatClosed = () => {
+  hiHatTop.style.top = "171px";
+};
+window.addEventListener("keydown", (event) => {
+  let code = event.keyCode;
+  let keyElement = document.querySelector(`div[data-key="${code}"]`);
+  if (!keyElement) return;
+
+  let audio = document.querySelector(`audio[data-key="${code}"]`);
+  audio.currentTime = 0;
+  audio.play();
+
+  switch (code) {
+    case 69:
+    case 82:
+      animateCrashOrRide();
+
+      break;
+    case 75:
+      animateHiHatClosed();
+  }
+});
